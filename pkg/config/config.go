bc97e1f9510f38c778e54427fb22245ca8c8ef0d
lucasew <lucas59356@gmail.com>
2026-02-27 23:00:27 -0300

    checkpoint

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -16,14 +16,39 @@ type Config struct {
 	Remotes map[string]RemoteConfig `toml:"remotes"`
 }
 
-func LoadConfig() (*Config, error) {
-	home, err := os.UserHomeDir()
+type ConfigLoader struct {
+	Location string
+}
+
+func (c *ConfigLoader) GetLocation() (string, error) {
+	if c.Location == "" {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+		confPath := filepath.Join(home, ".config", "mclone", "mclone.conf")
+		return confPath, err
+	}
+	_, err := os.Stat(c.Location)
+	if err == nil {
+		return c.Location, nil
+	}
+	if err == os.ErrNotExist {
+		f, err := os.Create(c.Location)
+		if err != nil {
+			return "", err
+		}
+		defer f.Close()
+	}
+	return c.Location, nil
+}
+
+func (c *ConfigLoader) Load() (*Config, error) {
+	configFile, err := c.GetLocation()
 	if err != nil {
 		return nil, err
 	}
-	confPath := filepath.Join(home, ".config", "mclone", "mclone.conf")
-
-	data, err := os.ReadFile(confPath)
+	data, err := os.ReadFile(configFile)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return &Config{Remotes: make(map[string]RemoteConfig)}, nil
@@ -38,20 +63,14 @@ func LoadConfig() (*Config, error) {
 	return &conf, nil
 }
 
-func (c *Config) Save() error {
-	home, err := os.UserHomeDir()
+func (c *ConfigLoader) Save(cfg *Config) error {
+	configFile, err := c.GetLocation()
 	if err != nil {
 		return err
 	}
-	confDir := filepath.Join(home, ".config", "mclone")
-	if err := os.MkdirAll(confDir, 0755); err != nil {
-		return err
-	}
-
-	data, err := toml.Marshal(c)
+	data, err := toml.Marshal(cfg)
 	if err != nil {
 		return err
 	}
-
-	return os.WriteFile(filepath.Join(confDir, "mclone.conf"), data, 0644)
+	return os.WriteFile(configFile, data, 0644)
 }
